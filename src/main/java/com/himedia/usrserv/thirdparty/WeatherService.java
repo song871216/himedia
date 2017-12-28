56580d69471619637c3cfc94e265e814c51374c1
songjiqing <574502657@qq.com>
2017-12-29 07:39:38 +0800

    start dev weather service

--- src/main/java/com/himedia/usrserv/thirdparty/WeatherService.java
+++ src/main/java/com/himedia/usrserv/thirdparty/WeatherService.java
@@ -1,9 +1,12 @@
 package com.himedia.usrserv.thirdparty;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -25,6 +28,11 @@ public class WeatherService {
 	@Autowired
 	WeatherDao weatherDao;
 	
+	/**
+	 * 按照城市名称模糊查询
+	 * @param cityName
+	 * @return
+	 */
 	public List<WeatherCityCode> findCitiesByName(String cityName) {
 		List<WeatherCityCode> weatherCityCodes = weatherDao.matchByCityName(cityName);
 		return weatherCityCodes;
@@ -32,10 +40,26 @@ public class WeatherService {
 	
 	
 	public Object getCurrentWeather(String cityCode) {
+		String reqUrl = createReqUrl(getCurrentWeatherUrl, cityCode, System.currentTimeMillis());
 		
 	}
 	
+	private String createReqUrl(String src, String cityCode, long ts) {
+		
+		return MessageFormat.format(src, cityCode, ts);
+	}
+
+
 	public Object getActivityWeather(String cityCode) {
 		
+	}
+	
+	private HttpEntity<String> getDefaultHeaders() {
+		HttpHeaders headers = new HttpHeaders();
+		headers.add("Content-Type", "text/html; charset=utf-8");
+		headers.add("Accept", "text/html");
+		headers.add("Referer", "http://mobile.weather.com.cn");
+		
+		return new HttpEntity<String>(headers);
 	}
 }
